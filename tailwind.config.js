ba74c6d989fdba1ae0c9795c96199067d9fac298
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2022-05-03 13:51:52 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'hd-3xl': '0px 16px 250px 10px rgba(155, 155, 155, 0.25)'
+      }
+    },
+  },
+  plugins: [],
+}
